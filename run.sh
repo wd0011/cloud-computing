4471fae6e5c72c1ef37d88fc6947ab6154f50096
wd0011 <34251317+wd0011@users.noreply.github.com>
2018-09-30 23:33:51 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
+hadoop jar $STREAM \  # this is the java-streaming path of hadoop
+-mapper ./mapper.py -file /home/weid0929/Downloads/mapper.py \ #this is the mapper.py file
+-reducer ./reducer.py -file /home/weid0929/Downloads/reducer.py \ # this is the reducer.py file
+-input /input/bible \ #dataset
+-output /output \ #this is the output path
+
 hadoop jar $STREAM \
 -mapper ./mapper_two_word.py -file /home/weid0929/Downloads/mapper_two_word.py \
 -reducer ./reducer.py -file /home/weid0929/Downloads/reducer.py \
 -input /input/bible \
 -output /output/two \
+
+hadoop jar $STREAM \
+-mapper ./mapper_match.py -file /home/weid0929/Downloads/mapper_match.py \
+-reducer ./reducer.py -file /home/weid0929/Downloads/reducer.py \
+-input /input/bible \
+-output /output/match \
